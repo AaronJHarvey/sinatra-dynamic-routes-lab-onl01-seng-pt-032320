fb35e941193a4aa84e47b301ee3c9eb73d198cff
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-24 19:51:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
    end 
    
    get '/say/:word1/:word2/:word3/:word4/:word5' do 
-     answer = params[:word1]
+     "answer = params[:word1]"
    end 
    
    get '/:operation/:number1/:number2' do 
