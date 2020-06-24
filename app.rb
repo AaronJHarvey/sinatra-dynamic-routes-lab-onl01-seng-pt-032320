abd682828176bf6428dfcd19cd46691ff4b2ccbd
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-24 19:33:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do 
-    phrase = params[:phrase]
-    print phrase * params[:number].to_i 
+    params[:number].to_i.times do 
   end
 end
